0ae3e7153245d3eacb44171bd5f319ca83bb5589
terrie9876 <terrie9876@gmail.com>
2019-02-03 16:09:06 -0800

    First commit

---
+++ app/src/main/java/com/example/user/broncobooks/ListingActivity.java
@@ -0,0 +1,18 @@
+package com.example.user.broncobooks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ListView;
+
+public class ListingActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listing);
+
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+    }
+}
---
+++ app/src/main/java/com/example/user/broncobooks/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.example.user.broncobooks;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+    public static final String DEBUG_TAG = "LOGIN_DEBUG";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onLogin(View view){
+        Intent intent = new Intent(this,ListingActivity.class);
+        Log.i(DEBUG_TAG,"Login button pressed");
+        startActivity(intent);
+
+    }
+}
