2fd47684255a2420cbb9ab0791a7e8c6669ff498
David <dct828@gmail.com>
2019-02-21 23:07:58 -0800

    Began working on google auth. Paused due to needed version updates requiring a redownload of google-services.json

--- app/src/main/java/com/example/user/broncobooks/LoginActivity.java
+++ app/src/main/java/com/example/user/broncobooks/LoginActivity.java
@@ -6,13 +6,34 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+    SignInButton googleButton;
+    FirebaseAuth mAuth;
+
+
     public static final String DEBUG_TAG = "LOGIN_DEBUG";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        googleButton = (SignInButton) findViewById(R.id.googleBtn);
     }
+
+    /*GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build();
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }*/
 
     public void onLogin(View view){
         Intent intent = new Intent(this,ListingActivity.class);
